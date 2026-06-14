e0ed2477a753b4622060cfe6482aca2e7153f3ff
nikita <nikit15110295@gmail.com>
2026-06-14 22:29:37 +0700

    edit Settings

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    LOG_SERVICE_URL: str = "http://192.54.100.195:6410"
+    LOG_SERVICE_URL: str = "http://logging-service:6410"
     LOG_SERVICE_API_KEY: str = "secret-dev-key"
     APP_VERSION: str = "1.0.0"
     ENVIRONMENT: str = "production"
